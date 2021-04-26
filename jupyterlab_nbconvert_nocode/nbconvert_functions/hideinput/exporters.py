3052c691bd59c66fe0ea6210252447696744e0ab
Vidar Tonaas Fauske <vidartf@gmail.com>
2021-04-26 18:27:55 +0100

    update templates to nbconvert 6

--- jupyterlab_nbconvert_nocode/nbconvert_functions/hideinput/exporters.py
+++ jupyterlab_nbconvert_nocode/nbconvert_functions/hideinput/exporters.py
@@ -5,18 +5,17 @@ from nbconvert.exporters.pdf import PDFExporter
 from .utils import ENV_VARS
 
 
-_html_no_code_template = os.environ.get(ENV_VARS['export_html'], '') or os.path.join(os.path.dirname(__file__), 'templates', 'hide_code_cells_html.tpl')
-_pdf_no_code_template = os.environ.get(ENV_VARS['export_pdf'], '') or os.path.join(os.path.dirname(__file__), 'templates', 'hide_code_cells_pdf.tplx')
-_html_no_code_email_template = os.environ.get(ENV_VARS['export_html_email'], '') or os.path.join(os.path.dirname(__file__), 'templates', 'hide_code_cells_html_email.tpl')
-
-
-def export_pdf(nbpath, template=_pdf_no_code_template):
+def export_pdf(nbpath, template=None):
     from nbconvert.nbconvertapp import NbConvertApp
+    if template is None:
+        template = os.environ.get(ENV_VARS['export_pdf'], 'hide_code_cells_pdf')
     NbConvertApp.launch_instance([nbpath, '--template', template, '--to', 'pdf'])
 
 
-def export_html(nbpath, template=_html_no_code_template):
+def export_html(nbpath, template=None):
     from nbconvert.nbconvertapp import NbConvertApp
+    if template is None:
+        template = os.environ.get(ENV_VARS['export_html'], 'hide_code_cells_html')
     NbConvertApp.launch_instance([nbpath, '--template', template, '--to', 'html'])
 
 
@@ -24,30 +23,12 @@ class HTMLHideCodeExporter(HTMLExporter):
     export_from_notebook = "HTML - No Code"
 
     # exclude_input = True
-    def _file_extension_default(self):
-        return '.html'
-
-    @property
-    def template_path(self):
-        if os.environ.get(ENV_VARS['export_html'], ''):
-            return super(HTMLHideCodeExporter, self).template_path + [os.path.dirname(os.environ.get(ENV_VARS['export_html']))]
-        return super(HTMLHideCodeExporter, self).template_path + [os.path.join(os.path.dirname(__file__), 'templates')]
-
-    def _template_file_default(self):
-        return 'hide_code_cells_html.tpl'
+    def _template_name_default(self):
+        return 'hide_code_cells_html'
 
 
 class PDFHideCodeExporter(PDFExporter):
     export_from_notebook = "PDF - No Code"
 
-    def _file_extension_default(self):
-        return '.pdf'
-
-    @property
-    def template_path(self):
-        if os.environ.get(ENV_VARS['export_pdf'], ''):
-            return super(PDFHideCodeExporter, self).template_path + [os.path.dirname(os.environ.get(ENV_VARS['export_pdf']))]
-        return super(PDFHideCodeExporter, self).template_path + [os.path.join(os.path.dirname(__file__), 'templates')]
-
     def _template_file_default(self):
-        return 'hide_code_cells_pdf.tplx'
+        return 'hide_code_cells_pdf'

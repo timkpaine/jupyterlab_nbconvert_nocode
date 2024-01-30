b0e8651f8cf8cc5008d1cb394dd92af1ef38ba24
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-01-30 14:58:36 -0500

    fix lints

--- jupyterlab_nbconvert_nocode/tests/test_all.py
+++ jupyterlab_nbconvert_nocode/tests/test_all.py
@@ -1,3 +1,7 @@
 class TestImport:
     def test_import(self):
-        from jupyterlab_nbconvert_nocode.nbconvert_functions import export_html, export_pdf
+        from jupyterlab_nbconvert_nocode.nbconvert_functions import (
+            export_html, export_pdf)
+
+        assert export_html is not None
+        assert export_pdf is not None
